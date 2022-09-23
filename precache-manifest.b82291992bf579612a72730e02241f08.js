bae5a307a86be84117e0a49fc558756e7ba46b75
Jean-Pierre Ledos <jpledos@gmail.com>
2022-09-23 11:35:10 +0200

    Updates

---
+++ precache-manifest.b82291992bf579612a72730e02241f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d79f28cc1309e48e578",
+    "url": "/cv/static/css/main.2ab268ea.chunk.css"
+  },
+  {
+    "revision": "3d79f28cc1309e48e578",
+    "url": "/cv/static/js/main.3d79f28c.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/cv/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "19f4449dd3287fc9cec0",
+    "url": "/cv/static/js/2.19f4449d.chunk.js"
+  },
+  {
+    "revision": "c21c6c25461046b3d584",
+    "url": "/cv/static/js/runtime~main.c21c6c25.js"
+  },
+  {
+    "revision": "6bd2c7757a4b50e077a475454d6aa8b7",
+    "url": "/cv/index.html"
+  }
+];
